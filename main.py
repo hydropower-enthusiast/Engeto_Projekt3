fe50b016d7d846280a04da17ded6cfee6ad0f16a
hydropower-enthusiast <harazinpremysl@gmail.com>
2025-07-15 20:39:18 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -27,25 +27,12 @@ def vyber_atributy_z_radku(
     
     return {a:td_tag[b].getText(), c:td_tag[d].getText(), e:td_tag[f].getText()}
 
-def naformatuj_odkaz(
-    cislo : str, 
-    kraj : str, 
-    numnuts : str,
-) -> str:
-    """
-    Funkce formatujici odkaz kazde stranky, ktera ma byt scrapovana.
-    Neplati pro Zahranici, jelikoz to je jinak strukturovano.
-    """
-    return (f"""https://www.volby.cz/pls/ps2017nss/ps311?xjazyk=CZ&xkraj={kraj}"""
-            f"""&xobec={cislo}&xnumnuts={numnuts}""")
-
 def scraper(
     url : str,
 ):
     """
     Funkce zajistujici scrapovani dat. Vyuziva knihoven requests a bs4.
     
-    :param - str
     """
     odp_serveru = get(url)
     odp_serveru.encoding='UTF-8'
@@ -72,111 +59,13 @@ def zisk_hodnot(
         vsechny_tr = table.find_all("tr")
         for tr in vsechny_tr[g:h]:
             td_na_radku = tr.find_all("td")
-            hodnoty_atributu = vyber_atributy_z_radku(td_na_radku, a, b, c, d, e, f)
+            hodnoty_atributu = vyber_atributy_z_radku(td_na_radku, a, 
+                                                      b, c, d, e, f)
             vysledky.append(hodnoty_atributu)
   
     return vysledky  
 
-def obec(
-    url : str
-) -> list:
-    
-    """
-    Scrapeovaci funkce fungujici na urovni konkretni obce.
-    Je rozdelena na dve casti, kdy prvni cast scrapne obecnou tabulku
-    uvadejici data o poctu Registrovanych, Obalek, a platnych hlasu a 
-    druhou cast, ktera scrapne volebni vysledky jednotlivych stran.
-    Obe casti jsou v jedne funkci, jelikoz data jsou na stejne strance a 
-    tedy je nutne jen jednou scrapovat.
-    """
-    
-    # Scrapnuti url pomoci soup
-    soup = scraper(url)
-
-    my_tag = "table"
-    my_att = {"id":"ps311_t1"}
-    a = "registrovanych"
-    b = 3
-    c = "obalky"
-    d = 4
-    e = "platnych"
-    f = 7
-    # Range 1
-    g = 2
-    # Range 2
-    h = None
-    # limit for find_all
-    i = None 
-    vysledky = zisk_hodnot(soup, my_tag, my_att, a, b, c, d, e, f, g, h, i)
-    
-    my_tag = "table"
-    my_att = {"class":"table"}
-    a = "poradi"
-    b = 0
-    c = "nazev_strany"
-    d = 1
-    e = "pocet_hlasu"
-    f = 2
-    # Range 1
-    g = 2
-    # Range 2
-    h = None
-    # limit for find_all
-    i = None
-    vysledky2 = zisk_hodnot(soup, my_tag, my_att, a, b, c, d, e, f, g, h, i)
-    
-    return vysledky, vysledky2[1:]
-    
-def main_function(
-    url : str,
-    kraj : str,
-    numnuts : str,
-) -> dict:
-    
-    """
-    Zajistuje scrapeovani na urovni uzemniho celku. 
-    Scrapne cisla vsech jednotlivych obci z uzemniho celku a nazev
-    vsech techto obci.
-    Nasledne tyto cisla pouzije pro ziskani a vytvoreni url odkazu
-    ktere jsou pouzity pro scrapeovani konkretnich volebnich dat
-    jednotlivych obci. Tyto data nasledne zapisuje do slovniku.
-    """
-
-    soup=scraper(url)
-    my_tag = "table"
-    my_att = {"class":"table"}
-    a = "Cislo"
-    b = 0
-    c = "název"
-    d = 1
-    e = "empty"
-    f = 2
-    # Range 1
-    g = 2
-    # Range 2
-    h = None
-    # limit for find_all
-    i = None
-    vysledky = zisk_hodnot(soup, my_tag, my_att, a, b, c, d, e, f, g, h, i)
-  
-    slovnik_konecnych_dat = {} 
-    for each in vysledky:
-        try:
-            y = obec(naformatuj_odkaz(each["Cislo"], kraj, numnuts))
-            slovnik_konecnych_dat[each["Cislo"]] = {"Location":each["název"],
-                              "Registered":y[0][0]["registrovanych"],
-                              "Envelopes":y[0][0]["obalky"],
-                              "Valid":y[0][0]["platnych"]}
-
-            for pskr in range(0,len(y[1])):
-                slovnik_konecnych_dat[each["Cislo"]][y[1][pskr]["nazev_strany"]] = y[1][pskr]["pocet_hlasu"]
-
-        except IndexError:
-            pass
-    
-    return slovnik_konecnych_dat
-
-def webpage_check_obecne(
+def list_of_webpages(
     soup : "bs4.BeautifulSoup", 
     a : str, 
     b : str, 
@@ -190,20 +79,20 @@ def webpage_check_obecne(
     stranek.
     """
    
-    # Vyhledani vsech linku/odkazu
-    project_href = [i['href'] for i in soup.find_all('a', href=True)]
-    project_href2 = []
+    # Vyhledani vsech linku/odkazu na dane strance
+    link_list = [i['href'] for i in soup.find_all('a', href=True)]
+    list_dovolenych_stranek = []
     # Cyklus ktery ze vsech linku vybere vsechny dovolene linky.
-    for each in project_href:
+    for each in link_list:
         if a and b in each or c in each:
             each="https://www.volby.cz/pls/ps2017nss/"+each
-            project_href2.append(each)
+            list_dovolenych_stranek.append(each)
     # Returnuje list vsech dovolenych linku.
-    return project_href2
+    return list_dovolenych_stranek
 
-
-def exceptional_case_zahranici(
+def main_function(
     url : str,
+    zahranici : bool,
 ) -> dict:
     
     """
@@ -219,49 +108,86 @@ def exceptional_case_zahranici(
     
     w = "xjazyk=CZ&xkraj=2&xobec=999997"
     x = "xokrsek"
-    y = "intentionally empty"
+    y = "xvyber"
     
-    project_zahranici = webpage_check_obecne(soup,w,x,y)
+    seznam_url = list_of_webpages(soup,w,x,y)
 
     my_tag = "table"
     my_att = {"class":"table"}
-    a = "Okrsek"
-    b = -1
-    c = "název"
-    d = -2
-    e = "empty"
-    f = -1
-    # Range 1
-    g = 1
-    # Range 2
-    h = None
-    # limit for find_all
-    i = None
-    vysledky = zisk_hodnot(soup, my_tag, my_att, a, b, c, d, e, f, g, h, i)
+    
+    if zahranici: 
+        a = "Okrsek"
+        b = -1
+        c = "název"
+        d = -2
+        e = "empty"
+        f = -1
+        # Range 1
+        g = 1
+        # Range 2
+        h = None
+        # limit for find_all
+        i = None
+
+    else:
+        a = "Cislo"
+        b = 0
+        c = "název"
+        d = 1
+        e = "empty"
+        f = 2
+        # Range 1
+        g = 2
+        # Range 2
+        h = None
+        # limit for find_all
+        i = None   
+    
+    okrsek_obec = a
+    vysledky_uz_celek = zisk_hodnot(soup, my_tag, my_att, 
+                                    a, b, c, d, e, f, g, h, i)
 
     slovnik_konecnych_dat = {} 
-    for i in range(0, len(vysledky)):
-            each = vysledky[i]
+    for i in range(0, len(vysledky_uz_celek)):
+            each = vysledky_uz_celek[i]
 
-            url = project_zahranici[i]
+            url = seznam_url[i]
             soup = scraper(url)
             
-            my_tag = "table"
-            my_att = {"class":"table"}
-            a = "registrovanych"
-            b = 0
-            c = "obalky"
-            d = 3
-            e = "platnych"
-            f = 4
-            # Range 1
-            g = 1
-            # Range 2
-            h = 2
-            # limit for find_all
-            i = 1
+            if zahranici:
+                my_tag = "table"
+                my_att = {"class":"table"}
+                a = "registrovanych"
+                b = 0
+                c = "obalky"
+                d = 3
+                e = "platnych"
+                f = 4
+                # Range 1
+                g = 1
+                # Range 2
+                h = 2
+                # limit for find_all
+                i = 1
+
+            else:
+                my_tag = "table"
+                my_att = {"id":"ps311_t1"}
+                a = "registrovanych"
+                b = 3
+                c = "obalky"
+                d = 4
+                e = "platnych"
+                f = 7
+                # Range 1
+                g = 2
+                # Range 2
+                h = None
+                # limit for find_all
+                i = None                 
             
-            vysledkyy = zisk_hodnot(soup, my_tag, my_att, a, b, c, d, e, f, g, h, i)
+            vysledky_region_1 = zisk_hodnot(soup, my_tag, my_att, 
+                                    a, b, c, d, e, f, g, h, i)
             
             my_tag = "table"
             my_att = {"class":"table"}
@@ -277,18 +203,27 @@ def exceptional_case_zahranici(
             h = None
             # limit for find_all
             i = None            
-            vysledky2 = zisk_hodnot(soup, my_tag, my_att, a, b, c, d, e, f, g, h, i)
+            vysledky_region_2 = zisk_hodnot(soup, my_tag, my_att, 
+                                    a, b, c, d, e, f, g, h, i)
+            if not zahranici:
+                vysledky_region_2 = vysledky_region_2[1:]
 
-            # Pozn: try/except - Index Error zde není řešen, jelikož 
-            # Zahraničí pro zadaný rok má v tabulkách všechny řádky obsazené.
-            y=[vysledkyy,vysledky2]            
-            slovnik_konecnych_dat[each["Okrsek"]] = {"Location":each["název"],
-                              "Registered":y[0][0]["registrovanych"],
-                              "Envelopes":y[0][0]["obalky"],
-                              "Valid":y[0][0]["platnych"]}
+            # Pozn: try/except - Index Error je řešen, jelikož 
+            # ne všechny územní celky mají v tabulkách na webové stránce
+            # všechny řádky obsazené. 
+            try:
+                y=[vysledky_region_1, vysledky_region_2]            
+                slovnik_konecnych_dat[each[okrsek_obec]] = {"Location":each["název"],
+                                  "Registered":y[0][0]["registrovanych"],
+                                  "Envelopes":y[0][0]["obalky"],
+                                  "Valid":y[0][0]["platnych"]}
+    
+                for iterator in range(0,len(y[1])):
+                    (slovnik_konecnych_dat[each[okrsek_obec]][y[1][iterator]
+                        ["nazev_strany"]]) = y[1][iterator]["pocet_hlasu"]
 
-            for pskr in range(0,len(y[1])):
-                slovnik_konecnych_dat[each["Okrsek"]][y[1][pskr]["nazev_strany"]] = y[1][pskr]["pocet_hlasu"]
+            except IndexError:
+                pass
 
     return slovnik_konecnych_dat
 
@@ -299,7 +234,10 @@ def csv_zapisovac(
     
     """
     zapisovani dat do souboru .csv
-    pouziva vygenerovany slovnik hodnot a nazev souboru zadany uzivatelem.
+    pouziva slovnik konecnych dat a nazev souboru zadany uzivatelem.
+    Je pouzit delimiter ; a encoding windows-1250, jelikož obsahuje 
+    českou abecedu. 
+    Mezera mezi tisíci a stovkami není řešena, dá se řešit snadno v excelu.
     """    
     
     hlavicka_helper = list(slovnik_konecnych_dat.keys())
@@ -340,25 +278,18 @@ def basic_function(url, file_name):
         print("incorrect positioning")
         sys.exit()
     # Kontrola, zda uzivatel zadal spravny odkaz na uzemni celek.
-    elif url not in webpage_check_obecne(soup, w, x, y):
+    elif url not in list_of_webpages(soup, w, x, y):
         print("incorrect webpage adress")
         sys.exit()
     # Uzemni celek Zahranici ma jako jediny odlisnou webovou strukturu
     # a proto ma sve vlastni reseni.
     elif url == "https://www.volby.cz/pls/ps2017nss/ps36?xjazyk=CZ":
-        slovnik_konecnych_dat = exceptional_case_zahranici(url)
+        slovnik_konecnych_dat = main_function(url, zahranici = True)
 
     # Vsechny ostatni uzemni celky jsou zpracovavany trochu jinym zpusobem, 
     # finalni vygenerovany slovnik ma vsak v obou pripadech stejnou strukturu.
     else:
-        numnuts = url[-4:]
-        kraj = url.split("xkraj=", 1)[1]
-        if kraj[0:2].isdigit():
-            kraj = kraj[0:2]
-        else:
-            kraj = kraj[0:1]
-            
-        slovnik_konecnych_dat = main_function(url, kraj, numnuts)
+        slovnik_konecnych_dat = main_function(url, zahranici = False)
     
     csv_zapisovac(slovnik_konecnych_dat, file_name)
     print("end")
